ab54cdd95ebb9ee179d406dccecd606698699d9f
marineteroitin <marine.teroitin@gmail.com>
2020-11-30 23:06:37 +0100

    variables liées au loginUI fxml + fonction display et initialize()

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,10 +4,45 @@ import facade.UserFacade;
 
 import java.util.*;
 
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import ui.Router;
+import javafx.event.ActionEvent;
 /**
  * 
  */
 public class LoginController {
+
+    // Java FX Implementation
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private TextField txtLoginUser;
+
+    @FXML
+    private PasswordField txtPassWord;
+
+    @FXML
+    private Label errorText;
+
+    // Config
+
+    private String login;
+    private String passWord;
+
+
+    /**
+     *
+     */
+    private UserFacade userFacade = new UserFacade();
 
     /**
      * Default constructor
@@ -15,10 +50,6 @@ public class LoginController {
     public LoginController() {
     }
 
-    /**
-     * 
-     */
-    private UserFacade userFacade;
 
 
     /**
@@ -35,4 +66,14 @@ public class LoginController {
         // TODO implement here
     }
 
+    @FXML
+    public void display(String msg)
+    {
+        errorText.setText(msg);
+    }
+
+    public void initialize() {
+        // TODO Auto-generated method stub
+        errorText.setText("");
+    }
 }
