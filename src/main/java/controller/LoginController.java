2319631d1ec2a771b32e2ff55ddb5612157f4767
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-12-01 09:04:06 +0100

    package router

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -8,8 +8,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import Router;
+import router.Router;
 import javafx.event.ActionEvent;
+
 /**
  * 
  */
