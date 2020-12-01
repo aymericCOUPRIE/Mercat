53f409315a63ac927e804c26eafa1a55b8450fd1
marineteroitin <marine.teroitin@gmail.com>
2020-12-01 09:30:27 +0100

    main incomplet

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,4 +1,45 @@
 package application;
 
-public class Main {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import router.Router;
+
+public class Main extends Application {
+
+    public static Stage primaryStage = null;
+
+    @Override
+    public void start(Stage primaryStage) {
+        Main.primaryStage = primaryStage;
+
+        try {
+            FXMLLoader root = new FXMLLoader(getClass().getResource("/resources/LoginUI.fxml"));
+            Parent skillLayout = root.load();
+            Scene scene = new Scene(skillLayout, 1000, 800);
+
+            primaryStage.setTitle("Mercat");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void init() {
+        //Router r = Router.getInstance();
+        /*r.add("HomePage", "../../resources/HomePage.fxml");
+        r.add("Login", "../../resources/LoginUI.fxml");
+        r.add("SignUp", "../../resources/SignUpUI.fxml");*/
+
+    }
+
 }
