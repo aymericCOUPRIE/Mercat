9589bebff4b74125f2a567f924292cc37a31c292
marineteroitin <marine.teroitin@gmail.com>
2020-12-01 09:00:44 +0100

    update

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,15 +2,13 @@ package controller;
 
 import facade.UserFacade;
 
-import java.util.*;
-
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import ui.Router;
+import Router;
 import javafx.event.ActionEvent;
 /**
  * 
