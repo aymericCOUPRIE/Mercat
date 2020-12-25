892c6f2758815eaaf27a4c6beda21764b42bb4ae
marineteroitin <marine.teroitin@gmail.com>
2020-12-25 14:26:02 -0500

    redirection page update consumer admin + début fonction delete consumer pour admin

--- src/main/java/controller/HandleConsumerController.java
+++ src/main/java/controller/HandleConsumerController.java
@@ -31,17 +31,20 @@ public class HandleConsumerController {
      * @return
      */
     public ArrayList<String> getAllConsumerPseudo() {
-        return UserFacade.getInstanceUserFacade().getAllConsumerPseudo();
+        return userFacade.getInstanceUserFacade().getAllConsumerPseudo();
 
         //afficher un message si vide ???????????
     }
 
-    /**
+    /** this method redirects the admin to the update consumer page whose pseudo is passed in parameter
      * @param pseudo 
-     * @return
+
      */
-    public void updateConsumer(Consumer pseudo) {
-        // TODO implement here
+    public void updateConsumer(String pseudo) {
+        Object[] params = new Object[1];
+        params[0] =  pseudo;
+        Router.getInstance().activate("HandleConsumer",params);
+
     }
 
     /**
@@ -49,7 +52,8 @@ public class HandleConsumerController {
      * @return
      */
     public void daleteConsumer(String pseudo) {
-        // TODO implement here
+        // TODO implement here -> voir si faut modifier fonction deleteUser et afficher un message sur la page pour confirmer la suppression
+        userFacade.getInstanceUserFacade().deleteUser(pseudo);
     }
 
     /**
