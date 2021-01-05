c660b8aefd1759a20ead7b6ad475b7201df663a0
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2021-01-05 12:23:28 +0100

    Update ProductDAOMySQL.java

--- src/main/java/dao/ProductDAOMySQL.java
+++ src/main/java/dao/ProductDAOMySQL.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.*;
 
 /**
- * 
+ *
  */
 public class ProductDAOMySQL extends ProductDAO {
 
@@ -22,7 +22,7 @@ public class ProductDAOMySQL extends ProductDAO {
     }
 
     /**
-     * @param idProduct 
+     * @param idProduct
      * @return
      */
     public Product getProduct(int idProduct) {
@@ -37,9 +37,9 @@ public class ProductDAOMySQL extends ProductDAO {
     public ArrayList<Product> getProductsByName(String nameP) {
         ArrayList<Product> listProduct = new ArrayList<Product>();
         String requete = "SELECT * FROM product WHERE nameProduct = ? ORDER BY nameProduct ASC";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,nameP);
+            preparedStatement.setString(1, nameP);
             return getProductList(listProduct, preparedStatement);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -55,10 +55,10 @@ public class ProductDAOMySQL extends ProductDAO {
     public ArrayList<Product> getProductsByNameAndCity(String name, String city) {
         ArrayList<Product> listProduct = new ArrayList<Product>();
         String requete = "SELECT * FROM product WHERE nameProduct = ? AND city = ? ORDER BY nameProduct ASC";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,name);
-            preparedStatement.setString(2,city);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, city);
             return getProductList(listProduct, preparedStatement);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -67,18 +67,18 @@ public class ProductDAOMySQL extends ProductDAO {
     }
 
     /**
-     * @param name 
+     * @param name
      * @param category
      * @return
      */
     public ArrayList<Product> getProductsByNameAndCategory(String name, String category) {
         ArrayList<Product> listProduct = new ArrayList<Product>();
         String requete = "SELECT * FROM product WHERE nameProduct = ? AND idCategorie = ? ORDER BY nameProduct ASC";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,name);
+            preparedStatement.setString(1, name);
             int id = getCategoryId(category);
-            preparedStatement.setInt(2,id);
+            preparedStatement.setInt(2, id);
             return getProductList(listProduct, preparedStatement);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -91,7 +91,7 @@ public class ProductDAOMySQL extends ProductDAO {
 
         Product product;
 
-        while(res.next()){
+        while (res.next()) {
             product = new Product(
                     res.getString("name"),
                     res.getString("description"),
@@ -106,18 +106,18 @@ public class ProductDAOMySQL extends ProductDAO {
     }
 
     /**
-     * @param name 
-     * @param city 
-     * @param idCategory 
+     * @param name
+     * @param city
+     * @param idCategory
      * @return
      */
     public ArrayList<Product> getProductsByNameAndCityAndCategory(String name, String city, int idCategory) {
         ArrayList<Product> listProduct = new ArrayList<Product>();
         String requete = "SELECT * FROM product WHERE nameProduct = ? AND idCategorie = ? ORDER BY nameProduct ASC";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,name);
-            preparedStatement.setString(2,""+idCategory);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, "" + idCategory);
             return getProductList(listProduct, preparedStatement);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -130,17 +130,17 @@ public class ProductDAOMySQL extends ProductDAO {
         //TODO Demander si cela marche
         //System.out.println("FIRST "+product.getDescription());
         String requete = "INSERT INTO product (name,description,price,seller,category) VALUES (?,?,?,?,?)";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,product.getNameProduct());
-            preparedStatement.setString(2,product.getDescription());
+            preparedStatement.setString(1, product.getNameProduct());
+            preparedStatement.setString(2, product.getDescription());
             System.out.println(product.getNameProduct());
-            preparedStatement.setString(3,""+product.getPriceProduct());
-            preparedStatement.setString(4,product.getPseudoSeller());
+            preparedStatement.setString(3, "" + product.getPriceProduct());
+            preparedStatement.setString(4, product.getPseudoSeller());
             //TODO voir si on ne change pas l'id pour le nom de la catégorie
-            preparedStatement.setString(5,"Informatique");
+            preparedStatement.setString(5, "Informatique");
 
-            return preparedStatement.executeUpdate()!=0;
+            return preparedStatement.executeUpdate() != 0;
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -152,12 +152,12 @@ public class ProductDAOMySQL extends ProductDAO {
     public boolean updateProduct(Product p, String newDescription) {
         int id = getProductId(p);
         String requete = "UPDATE product SET description = ? WHERE idProduct = ?";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,newDescription);
-            preparedStatement.setString(2,""+id);
+            preparedStatement.setString(1, newDescription);
+            preparedStatement.setString(2, "" + id);
 
-            return preparedStatement.executeUpdate()!=0;
+            return preparedStatement.executeUpdate() != 0;
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -167,19 +167,20 @@ public class ProductDAOMySQL extends ProductDAO {
     }
 
     @Override
-    public int getProductId(Product p){
+    public int getProductId(Product p) {
         String requete = "SELECT idProduct FROM product WHERE nameProduct=? AND description=? AND price=? AND seller=? AND category=?";
 
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
             preparedStatement.setString(1, p.getNameProduct());
-            preparedStatement.setString(2,p.getDescription());
-            preparedStatement.setString(3,""+p.getPriceProduct());
-            preparedStatement.setString(4,p.getPseudoSeller());
-            preparedStatement.setString(5,p.getCategory());
+            preparedStatement.setString(2, p.getDescription());
+            preparedStatement.setString(3, "" + p.getPriceProduct());
+            preparedStatement.setString(4, p.getPseudoSeller());
+            //TODO ERREUR ERROR modif pour passer du nom à un int
+            preparedStatement.setString(5, String.valueOf(p.getIdCategorie()));
 
             ResultSet rs = preparedStatement.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 return rs.getInt("idProduct");
             }
             return -1;
@@ -189,31 +190,32 @@ public class ProductDAOMySQL extends ProductDAO {
         }
     }
 
-    public int getCategoryId(String c){
-        String requete = "SELECT idCategorie FROM Categorie WHERE LibelleCategorie = ?";
-        try{
+    public int getCategoryId(String c) {
+        String requete = "SELECT idCategorie FROM categorie WHERE LibelleCategorie = ?";
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,c);
+            preparedStatement.setString(1, c);
             ResultSet rs = preparedStatement.executeQuery();
 
-            if(rs.next()){
+            if (rs.next()) {
                 return rs.getInt("idCategorie");
             }
             return -1;
-        }catch(SQLException throwables){
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
             return -1;
         }
     }
+
     @Override
     public boolean deleteProduct(Product p) {
         int id = getProductId(p);
         String requete = "DELETE FROM product WHERE idProduct=?";
         try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,""+id);
+            preparedStatement.setString(1, "" + id);
 
-            return preparedStatement.executeUpdate()!=0;
+            return preparedStatement.executeUpdate() != 0;
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -221,19 +223,19 @@ public class ProductDAOMySQL extends ProductDAO {
         }
     }
 
-    private String getProductCity(String seller){
+    private String getProductCity(String seller) {
         String requete = "SELECT city FROM seller WHERE pseudo = ?";
-        try{
+        try {
             PreparedStatement preparedStatement = this.connect.prepareStatement(requete);
-            preparedStatement.setString(1,seller);
+            preparedStatement.setString(1, seller);
 
             ResultSet rs = preparedStatement.executeQuery();
 
-            if(rs.next()){
+            if (rs.next()) {
                 return rs.getString("city");
             }
             return "-1";
-        }catch(SQLException throwables){
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
             return "-1";
         }
