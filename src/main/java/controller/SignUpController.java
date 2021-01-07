6f42b17a84e6ac1e1735a0a107618a7acc3707aa
FlorianAudiger <audiger.florian@gmail.com>
2021-01-07 15:41:14 +0100

    Seller sign up verification

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -72,10 +72,6 @@ public class SignUpController {
 
     private String messageAttention;
 
-    //TODO delete ça flo quand t'aura utilisé la fonction dans utils -> checkinfoUser pour éviter dupliquer code de base d'anna
-    private static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-
     private boolean isAConsumer = true;
 
 
@@ -127,10 +123,13 @@ public class SignUpController {
         firstName = txtFirstname.getText();
         lastName = txtLastname.getText();
 
+        String check = CheckInfosUser.checkInfosSeller(pseudo,email,firstName, lastName,password,phoneNumber,street,postal,city,companyName);
 
-
-        if (checkInfosSeller() != false) {
+        if(check.equals("OK")){
             signUp(e);
+        }else{
+
+            display(check);
         }
 
     }
@@ -181,62 +180,6 @@ public class SignUpController {
     @FXML
     public void backLogin(ActionEvent e) throws IOException {
         Router.getInstance().activate("Login");
-    }
-
-    //TODO fonction à supprimer Flo quand t'aura utilisé la fonction dans utils -> checkinfoUser pour éviter dupliquer code de base d'anna
-
-    private boolean checkInfosConsumer() {
-        if ((pseudo.equals("") || password.equals("") || phoneNumber.equals("") || street.equals("") || postal.equals("") || city.equals(""))) {
-            this.messageAttention = "You need to provide every information";
-            display(messageAttention);
-            return false;
-
-        } else
-            return internVerification();
-    }
-
-//TODO fonction à supprimer Flo quand t'aura utilisé la fonction dans utils -> checkinfoUser pour éviter dupliquer code de base d'anna
-
-    private boolean internVerification() {
-        if (password.length() < 8) {
-            this.messageAttention = "Your password must have at least 8 characters";
-            display(messageAttention);
-        } else {
-            if(passwordNotNull(password)) {
-                if (phoneNumber.matches("[0-9]+") && (phoneNumber.length() == 10 || phoneNumber.length() == 12)) {
-                    Matcher matcher = pattern.matcher(email);
-                    if (matcher.matches()) {
-                        if(postal.matches("[0-9]+")&&(postal.length()==5)){
-                            return true;
-                        }else{
-                            this.messageAttention = "Your postal code is invalid";
-                            display(messageAttention);
-                        }
-                    } else {
-                        this.messageAttention = "Your email is incorrect";
-                        display(messageAttention);
-                    }
-                } else {
-                    this.messageAttention = "Your phone number is incorrect";
-                    display(messageAttention);
-                }
-            }else{
-                this.messageAttention = "You cannot have a space in the password" ;
-                display(messageAttention);
-            }
-        }
-        return false;
-    }
-
-    //TODO à modifier flo -> inspire toi de consumer, vérifie que ton param en plus est pas vide et après utilise ma fonction :)
-    private boolean checkInfosSeller() {
-        if ((pseudo.equals("") && password.equals("") || phoneNumber.equals("") && street.equals("") && postal.equals("") && city.equals("") && companyName.equals(""))) {
-            this.messageAttention = "You need to provide every information";
-            display(messageAttention);
-            return false;
-
-        } else
-            return internVerification();
     }
 
     private boolean passwordNotNull(String p){
