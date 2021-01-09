1f0df1ca6250f94dd4d8372ce0979ee469f67d12
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2021-01-09 17:01:47 +0100

    update test

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,20 +1,20 @@
 import dao.UserDAOMySQL;
+import dao.abstraction.UserDAO;
 import model.User;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 public class TestExample {
 
-    @Mock
-    User user;
 
-    @Test
-    public void testUserPseudo() {
-    }
+
 
 
 }
