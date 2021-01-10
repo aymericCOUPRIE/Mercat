dabdfa66f4c2b24a7664f32b117e2ff2cd719294
FlorianAudiger <audiger.florian@gmail.com>
2021-01-10 14:32:41 +0100

    Java doc

--- src/main/java/dao/abstraction/RateDAO.java
+++ src/main/java/dao/abstraction/RateDAO.java
@@ -19,9 +19,9 @@ public abstract class RateDAO {
     private static RateDAO instanceRateDAO;
 
     /**
-     * Default constructor
+     * this methode permit to  connect the dao with the database
      *
-     * @param connect
+     * @param connect is the connection for the database
      */
     protected RateDAO(Connection connect) {
         this.connect = connect;
@@ -36,54 +36,49 @@ public abstract class RateDAO {
     }
 
     /**
-     * @param product
-     * @return
+     * This method add a rate for a seller in the database
+     * @param seller
+     * @param consumer that add the rate
+     * @param rate an integer between 1 and 5
      */
+    public abstract void createRateSeller(Seller seller, Consumer consumer, int rate);
+
+    /**
+     * This method add a rate for a product in the database
+     * @param consumer that add the rate
+     * @param rate an integer between 1 and 5
+     * @param idProduct of the product
+     */
+    public abstract void createRateProduct(Consumer consumer, int rate, int idProduct);
+
+    /**
+     * This method get a rate of a seller in the database
+     * @param seller
+     * @param consumer who add the rate
+     * @return the rate put by the consumer, if no notation return 0
+     */
+    public abstract float rateSeller(Seller seller, Consumer consumer);
+
+    /**
+     * This method get a rate of a product in the database
+     * @param consumer who add the rate
+     * @param id of the product
+     * @return the rate put by the consumer, if no notation return 0
+     */
+    public abstract float rateProduct(Consumer consumer, int id);
+
+    /**
+     * This method get the average rate of all rates of a seller in the database
+     * @param seller
+     * @return the average rate of a seller, if no notation return 0
+     */
+    public abstract float averageRateSeller(Seller seller);
+
+
     public Set<Rate> getAllRatesProduct(Product product) {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @param seller
-     * @param consumer
-     * @param rate
-     */
-    public abstract void createRateSeller(Seller seller, Consumer consumer, int rate);
-
-    /**
-     * @param consumer
-     * @param rate
-     * @param idProduct
-     */
-    public abstract void createRateProduct(Consumer consumer, int rate, int idProduct);
-
-    /**
-     * @param seller
-     * @param consumer
-     * @return the rate put by the consumer
-     */
-    public abstract float rateSeller(Seller seller, Consumer consumer);
-
-    /**
-     * @param consumer
-     * @return the rate put by the consumer
-     */
-    public abstract float rateProduct(Consumer consumer, int i);
-
-    /**
-     * @param seller
-     * @return the average rate of a seller
-     */
-    public abstract float averageRateSeller(Seller seller);
-
-    /**
-     * @param seller
-     * @return
-     */
-    public Set<Rate> getAllRatesSeller(Seller seller) {
-        // TODO implement here
-        return null;
-    }
 
 }
